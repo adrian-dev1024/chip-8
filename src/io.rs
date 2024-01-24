bc86032a8d1af6ad653703610b29a5c64997f969
Adrian Garay <adrian.dev.1024@gmail.com>
2024-01-24 15:05:40 -0500

    big refactor

--- src/io.rs
+++ src/io.rs
@@ -6,6 +6,8 @@ use sdl2::render::WindowCanvas;
 use sdl2::video::Window;
 use sdl2::{EventPump, Sdl};
 
+use crate::chip8::ChipState;
+
 const DISPLAY_WIDTH: u16 = 64;
 const DISPLAY_HEIGHT: u16 = 32;
 const SCALE: u16 = 10;
@@ -52,6 +54,19 @@ impl Renderer {
         Ok(Renderer { canvas })
     }
 
+    pub fn draw(&mut self, gfx: [u8; 64 * 32]) -> Result<(), String> {
+        self.clear();
+        for (i, pix) in gfx.iter().enumerate() {
+            if *pix == 1 {
+                let x = i % 64;
+                let y = i / 64;
+                self.draw_dot(x, y)?;
+            }
+        }
+        self.canvas.present();
+        Ok(())
+    }
+
     pub fn clear(&mut self) {
         self.canvas.set_draw_color(BACKGROUND_COLOR);
         self.canvas.clear();
@@ -75,88 +90,91 @@ impl Renderer {
             (height * SCALE).try_into().unwrap(),
         ))?;
 
-        self.canvas.present();
+        // self.canvas.present();
 
         Ok(())
     }
 
-    pub fn draw_dot(&mut self, x: usize, y: usize) -> Result<(), String> {
+    fn draw_dot(&mut self, x: usize, y: usize) -> Result<(), String> {
         self.draw_rect(x, y, 1, 1, DRAWING_COLOR)?;
         Ok(())
     }
-
-    pub fn clear_dot(&mut self, x: usize, y: usize) -> Result<(), String> {
-        self.draw_rect(x, y, 1, 1, BACKGROUND_COLOR)?;
-        Ok(())
-    }
-}
-
-// #[derive(Copy, Clone)]
-enum Keys {
-    Num1 = 0,
-    Num2 = 1,
-    Num3 = 2,
-    Num4 = 3,
-    Q = 4,
-    W = 5,
-    E = 6,
-    R = 7,
-    A = 8,
-    S = 9,
-    D = 10,
-    F = 11,
-    Z = 12,
-    X = 13,
-    C = 14,
-    V = 15,
 }
 
 pub struct Keyboard {
     event_pump: EventPump,
-    pub quit: bool,
 }
 
 impl Keyboard {
     pub fn new(sdl_context: Sdl) -> Keyboard {
         let event_pump = sdl_context.event_pump().unwrap();
-        Keyboard {
-            event_pump,
-            quit: false,
-        }
+        Keyboard { event_pump }
     }
 
-    pub fn keys_pressed(&mut self, keys: &mut [u16; 16]) {
-        keys.fill(0);
+    pub fn keys_pressed(&mut self, keys: &mut [u8; 16], state: &mut ChipState) {
         for event in self.event_pump.poll_iter() {
             match event {
-                Event::Quit { .. } => self.quit = true,
+                Event::Quit { .. } => *state = ChipState::Quit,
                 Event::KeyDown {
                     keycode: Some(keycode),
                     ..
                 } => match keycode {
-                    Keycode::Num1 => keys[Keys::Num1 as usize] = 1,
-                    Keycode::Num2 => keys[Keys::Num2 as usize] = 1,
-                    Keycode::Num3 => keys[Keys::Num3 as usize] = 1,
-                    Keycode::Num4 => keys[Keys::Num4 as usize] = 1,
+                    Keycode::Num1 => keys[0] = 1,
+                    Keycode::Num2 => keys[1] = 1,
+                    Keycode::Num3 => keys[2] = 1,
+                    Keycode::Num4 => keys[3] = 1,
 
-                    Keycode::Q => keys[Keys::Q as usize] = 1,
-                    Keycode::W => keys[Keys::W as usize] = 1,
-                    Keycode::E => keys[Keys::E as usize] = 1,
-                    Keycode::R => keys[Keys::R as usize] = 1,
+                    Keycode::Q => keys[4] = 1,
+                    Keycode::W => keys[5] = 1,
+                    Keycode::E => keys[6] = 1,
+                    Keycode::R => keys[7] = 1,
 
-                    Keycode::A => keys[Keys::A as usize] = 1,
-                    Keycode::S => keys[Keys::S as usize] = 1,
-                    Keycode::D => keys[Keys::D as usize] = 1,
-                    Keycode::F => keys[Keys::F as usize] = 1,
+                    Keycode::A => keys[8] = 1,
+                    Keycode::S => keys[9] = 1,
+                    Keycode::D => keys[10] = 1,
+                    Keycode::F => keys[11] = 1,
 
-                    Keycode::Z => keys[Keys::Z as usize] = 1,
-                    Keycode::X => keys[Keys::X as usize] = 1,
-                    Keycode::C => keys[Keys::C as usize] = 1,
-                    Keycode::V => keys[Keys::V as usize] = 1,
+                    Keycode::Z => keys[12] = 1,
+                    Keycode::X => keys[13] = 1,
+                    Keycode::C => keys[14] = 1,
+                    Keycode::V => keys[15] = 1,
+                    Keycode::Space => {
+                        *state = if *state == ChipState::Pause {
+                            ChipState::Run
+                        } else {
+                            ChipState::Pause
+                        }
+                    }
+                    _ => {}
+                },
+                Event::KeyUp {
+                    keycode: Some(keycode),
+                    ..
+                } => match keycode {
+                    Keycode::Num1 => keys[0] = 0,
+                    Keycode::Num2 => keys[1] = 0,
+                    Keycode::Num3 => keys[2] = 0,
+                    Keycode::Num4 => keys[3] = 0,
+
+                    Keycode::Q => keys[4] = 0,
+                    Keycode::W => keys[5] = 0,
+                    Keycode::E => keys[6] = 0,
+                    Keycode::R => keys[7] = 0,
+
+                    Keycode::A => keys[8] = 0,
+                    Keycode::S => keys[9] = 0,
+                    Keycode::D => keys[10] = 0,
+                    Keycode::F => keys[11] = 0,
+
+                    Keycode::Z => keys[12] = 0,
+                    Keycode::X => keys[13] = 0,
+                    Keycode::C => keys[14] = 0,
+                    Keycode::V => keys[15] = 0,
                     _ => {}
                 },
                 _ => {}
             }
         }
+        // println!("keys: {:?}", keys);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,38 +5,17 @@ use chip8::Chip8;
 use io::IOContext;
 
 pub fn main() -> Result<(), String> {
+    let path_str = std::env::args().nth(1).expect("no path given");
+    let path = std::path::PathBuf::from(path_str);
+
     // Screen setup (sdl2)
     let mut io_context = IOContext::new()?;
 
     // Initialize the Chip8 system and load the game into the memory
-    let mut chip8 = Chip8::new(io_context.renderer);
-    chip8.load_game("/Users/adriangaray/workspace/rust/chip-8/games/tetris.c8")?;
+    let mut chip8 = Chip8::new();
+    chip8.load_game(path);
 
-    'running: loop {
-        chip8.emulate_cycle()?;
-
-        io_context.keyboard.keys_pressed(&mut chip8.keys);
-
-        if io_context.keyboard.quit {
-            break 'running;
-        }
-
-        // for event in event_pump.poll_iter() {
-        //     match event {
-        //         Event::Quit { .. }
-        //         | Event::KeyDown {
-        //             keycode: Some(Keycode::Escape),
-        //             ..
-        //         } => break 'running,
-        //         _ => {}
-        //     }
-        // }
-
-        // io_context.renderer.clear();
-        // io_context.renderer.present();
-        // ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
-        // The rest of the game loop goes here...
-    }
+    chip8.run_loop(&mut io_context)?;
 
     Ok(())
 }
